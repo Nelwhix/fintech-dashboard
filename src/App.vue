a06e7db355976724ea8ff64a010d9fb75ee4de75
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2023-01-04 00:03:04 +0100

    working on analytics section

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,26 @@
   import Sidebar from './components/Sidebar.vue';
   import { Bars4Icon } from '@heroicons/vue/24/outline'
   import Calendar from './components/Calendar.vue';
+  import FlowChart from './assets/FlowChart.vue'
+
+  const cardContents = [
+    {
+      title: "Total Income",
+      amount: "+ $25,0000.00"
+    },
+    {
+      title: "Total Expenses",
+      amount: "- $ 15,000.00"
+    },
+    {
+      title: "Total Saved",
+      amount: "- $ 3,000.00"
+    },
+    {
+      title: "Projections",
+      amount: "- $ 5,000.00"
+    }
+  ]
 </script>
 
 <template>
@@ -11,8 +31,20 @@
       <Bars4Icon class="w-6 h-6"/>
     </button> -->
 
-    <section class="col-span-6">
+    <section class="col-span-4">
       <Calendar />
+
+      <div class="flex font-primary justify-around mt-5">
+        <div v-for="card in cardContents" class="border border-gray-200 rounded-md text-sm p-3">
+          <h3>{{ card.title }}</h3>
+          <p class="font-medium">{{ card.amount }}</p>
+        </div>
+      </div>
+
+      <FlowChart class="w-[35rem] mx-auto" />
+    </section>
+    <section class="col-span-2">
+
     </section>
   </main>
  
