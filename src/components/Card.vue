0d1538701c25389efccb5acafdc71a1e2a618b2c
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2023-01-08 00:04:44 +0100

    finished working on card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -8,7 +8,7 @@ import MasterCardIcon from '../assets/MastercardIcon.vue'
 <template>
   <div class="w-72 h-48 mt-5 mx-auto card">
     <div class="flip">
-      <div class="front hidden">
+      <div class="front">
         <EllipseIcon class="absolute w-40 h-40 top-3 left-20" />
         <div class="flex justify-between mt-5 mx-4">
             <span class="font-semibold text-white"> 
@@ -27,7 +27,7 @@ import MasterCardIcon from '../assets/MastercardIcon.vue'
         <div class="flex justify-between mt-3 mx-4">
             <div>
                 <p class="text-[10px] text-white gap-2 flex">
-                    <span class="uppercase">valid thru</span>
+                    <span class="uppercase">valid till</span>
                     <span>06/24</span>
                 </p>
                 <p class="text-white">Nelson Isioma</p>
@@ -36,10 +36,10 @@ import MasterCardIcon from '../assets/MastercardIcon.vue'
         </div>
       </div>
       <div class="back">
-        <div class="strip-black"></div>
-        <div class="ccv text-white">
-          <label>CVV</label>
-          <div>123</div>
+        <div class="bg-black w-full h-10 mt-6"></div>
+        <div class="text-white w-full mt-3">
+          <p class="w-64 text-right text-xs">CVV</p>
+          <p class="bg-white text-right text-black rounded-md mx-3 mt-3 py-2 pr-2">123</p>
         </div>
     </div>
     </div>
@@ -65,6 +65,7 @@ import MasterCardIcon from '../assets/MastercardIcon.vue'
     }
 
     .front, .back {
+        position: absolute;
         width: inherit;
         height: inherit;
         border-radius: 15px;
@@ -77,4 +78,41 @@ import MasterCardIcon from '../assets/MastercardIcon.vue'
     .front {
         transform: translateZ(0);
     }
+
+    .card {
+        perspective: 1000px;
+    }
+
+    .card:hover .flip {
+        transform: rotateY(180deg);
+    }
+
+    .back {
+        transform: rotateY(180deg) translateZ(0);
+    }
+
+@-moz-keyframes flip {
+  0%, 100% {
+    transform: rotateY(0deg);
+  }
+  50% {
+    transform: rotateY(180deg);
+  }
+}
+@-webkit-keyframes flip {
+  0%, 100% {
+    transform: rotateY(0deg);
+  }
+  50% {
+    transform: rotateY(180deg);
+  }
+}
+@-o-keyframes flip {
+  0%, 100% {
+    transform: rotateY(0deg);
+  }
+  50% {
+    transform: rotateY(180deg);
+  }
+}
 </style>
